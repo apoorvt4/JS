5edebe6853736d38c19c60f18373db46f9d73d24
Apoorv Tiwari <105799909+apoorvt4@users.noreply.github.com>
2024-02-10 23:24:49 +0530

    Add files via upload

---
+++ 03_conversionOperations.js
@@ -0,0 +1,15 @@
+let score = false
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
