7e3f69391647d6d7fcb27c4f1f3af9637fb820c1
Apoorv Tiwari <105799909+apoorvt4@users.noreply.github.com>
2024-02-10 23:01:30 +0530

    Add files via upload

---
+++ 01_varibles.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "apoorvtiwari292@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId = 2 // Not allowed like that use const
+
+accountEmail = "hc@hc.com"
+accountPassword = "565656"
+accountCity = "Bengaluru"
+let accountState;
+
+console.log(accountId);
+console.table([accountId, accountPassword, accountEmail, accountCity, accountState])
---
+++ 02_dataTypes.js
@@ -0,0 +1,8 @@
+//alert(hello)
+
+console.log(3 + 3);
+console.log("Hitesh");
+
+let name = "Apoorv"
+let age = "21"
+let isLoggedIn = false
